f3c74fecff5fcaf84ec18a8fbf6e16d485c27cb4
harshitasaraf <69836099+harshitasaraf@users.noreply.github.com>
2020-10-13 15:57:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var day = prompt("Enter a weekday")
+switch(day){
+    case "Monday" :
+        console.log("today is monday")
+        break;
+    case "Tuesday" : 
+        console.log("today is tuesday")
+        break;
+    case "Wednesday" :
+        console.log("today is wednesday")
+        break;
+    case "Thursday" :
+        console.log("today is thursday")
+        break;
+    case "Friday" :
+            console.log("today is Friday")
+            break;
+    default :
+        console.log("have a beautiful day")
+        break;
+}
+
+
