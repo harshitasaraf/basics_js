838460cca728aa4be86be251e9c2ac8afd388462
harshitasaraf <69836099+harshitasaraf@users.noreply.github.com>
2020-10-13 15:14:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//trying out the conditional statements
+var numA = 7
+if(numA == 7){
+   console.log("try adding or subtracting some values")
+}
+else if (numA == 9){
+   console.log("this is fantastic")
+}
+
